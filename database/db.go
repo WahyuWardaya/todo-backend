8e24de8650d178c8b6cebab93f72b5f285f0d112
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-09-20 13:16:04 +0800

    Menambahkan inisialisasi koneksi database MySQL

--- database/db.go
+++ database/db.go
@@ -1,2 +1,29 @@
-package database
+package db
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	dsn := "root:@tcp(localhost:3306)/todolist_db" 
+	
+	var err error
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Gagal membuka koneksi: %v", err)
+	}
+
+	// tes koneksi
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Gagal koneksi ke database: %v", err)
+	}
+
+	fmt.Println("Berhasil koneksi ke database MySQL di localhost!")
+}
